5af819a7abf074838dacd4c83c2822f302217a41
mdjonestwo <mdjones2@protonmail.com>
2021-02-22 09:25:29 -0500

    added Bulma framework & SASS

---
+++ mdjonestwo-portfolio/src/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./App.sass";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">Bulma</h1>
+      <p className="subtitle">
+        Modern CSS framework based on{" "}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+          Flexbox
+        </a>
+      </p>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Input" />
+        </div>
+      </div>
+
+      <div className="field">
+        <p className="control">
+          <span className="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div className="buttons">
+        <a className="button is-primary">Primary</a>
+        <a className="button is-link">Link</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
